4250e0b2a4cc2f941bf2b2cdb7572a2b04002e53
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2021-07-02 21:50:41 +0200

    Feld zentriert

--- common/src/commonMain/kotlin/com/toleno/common/Feld.kt
+++ common/src/commonMain/kotlin/com/toleno/common/Feld.kt
@@ -3,17 +3,18 @@ package com.toleno.common
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 typealias Zellen = List<Spieler?>
 
 @Composable
 internal fun Feld(zellen: Zellen, onClick: (zelle: Int) -> Unit, enabled: Boolean) {
-	Column {
+	Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
 		repeat(3) { zeile ->
-			Row(horizontalArrangement = Arrangement.SpaceBetween) {
+			Row(horizontalArrangement = Arrangement.Center) {
 				repeat(3) { spalte ->
 					val zelle = zeile * 3 + spalte
 					Zelle(zellen[zelle], onClick = { onClick(zelle) }, enabled && zellen[zelle] == null)
